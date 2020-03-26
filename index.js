867d17550af0833e075ff5ad5702a1b97b679bf8
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-03-26 12:56:46 -0700

    ProjectManager Task complete

--- index.js
+++ index.js
@@ -148,8 +148,8 @@ class Instructor extends Lambdasian {
   demo(subject){
     return `Today we are learning about ${subject}`;
   }
-  grade(student, subject){
-    return `${student} receives a perfect score on ${subject}`
+  grade(Student, subject){
+    return `${Student.name} receives a perfect score on ${subject}`
   }
 }
 
@@ -184,7 +184,6 @@ class Student extends Lambdasian {
   sprintChallenge(subject){
     return `${this.name} has begun spring challenge on ${subject}`
   }
-
 }
 
 /*
@@ -200,9 +199,21 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(pmAttributes){
+    super(pmAttributes)
+    this.gradClassName = pmAttributes.gradClassName;
+    this.favInstructor = pmAttributes.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
+
+
 
 /*
   STRETCH PROBLEM (no tests!)
